71f5fa811b5c9ef0da30fad5ad8d2de7152bb462
ktleary <kevin@ktleary.com>
2018-06-17 19:04:42 -0400

    Updating background, removing extra css

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,12 @@ import * as React from 'react';
 
 import './App.css';
 import CreateMetaData from './components/create-metadata';
-const appStyles = {
-  title: {
-    color: '#1D1D1B',
-    fontSize: 24
-  }
-};
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <div style={appStyles.title}>
-          <CreateMetaData />
-        </div>
+        <CreateMetaData />
       </div>
     );
   }
